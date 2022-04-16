cc776c6da993cdc822c37b8fc43b0643b439d499
wtals <21800664@handong.ac.kr>
2022-04-16 18:41:56 +0900

    update 기능 구현

--- product.c
+++ product.c
@@ -23,3 +23,22 @@ int createProduct(Product *p){
 void readProduct(Product p){
     printf("%s\t\t%s\t\t%s\t\t%d\t\t%d\n", p.productName, p.productExplain, p.productWeight, p.price, p.way);
 }       //상품 하나를 출력하는 함수
+int updateProduct(Product *p){
+    printf("상품의 이름은? ");
+    getchar();
+    scanf("%[^\n]s",p->productName);
+    getchar();
+    printf("상품의 설명은? ");
+    scanf("%[^\n]s",p->productExplain);
+    getchar();
+    printf("상품의 무게는? ");
+    scanf("%[^\n]s",p->productWeight);
+    getchar();
+    printf("상품의 가격은? ");
+    scanf("%d",&p->price);
+    printf("상품의 배송방법은?(1.새벽배송/2.택배배송)");
+    scanf("%d",&p->way);
+    printf("=> 수정됨!\n");
+    return 1;
+}       //상품을 수정하는 함수
+
